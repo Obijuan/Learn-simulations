0c8853a6ea1422b9594c33e0f96d0d8c1693757c
obijuan <obijuan.cube@gmail.com>
2025-04-12 13:56:00 +0200

    Log: Add example 12: baldosa

---
+++ Log/Examples/12-baldosa.py
@@ -0,0 +1,55 @@
+"""
+  Dibujar un suelo en 3D con un sistema de coordenadas
+  Cambiar el color del fondo del escenario
+"""
+
+import numpy as np
+from fury import actor, window
+
+# ------- Constantes
+ORIGEN = np.array([0, 0, 0])
+
+# -- Color blanco con transparencia
+GREY = np.array([0.6, 0.6, 0.6])
+YELLOW = np.array([1, 1, 0])
+
+# -- Dimensiones del suelo
+GROUND_WIDTH = 5
+GROUND_DEPTH = 5
+GROUND_HEIGHT = 0.05
+
+# -- Tamaño de la ventana
+PANTALLA = (600, 400)
+
+# ------- Construir los objetos a visualizar
+centers = np.array([ORIGEN])
+ground_colors = np.array([GREY])
+ground_size = np.array([GROUND_WIDTH, GROUND_HEIGHT, GROUND_DEPTH])
+
+# --- Crear el suelo
+suelo = actor.cube(centers, colors=ground_colors, scales=ground_size)
+suelo.SetPosition(GROUND_WIDTH/2, -GROUND_HEIGHT/2, GROUND_DEPTH/2)
+
+# --- Crear el sistema coordenado
+ejes = actor.axes()
+
+# -- Crear la baldosa amarilla de 1x1
+baldosa_size = np.array([1, 0.1, 1])
+centers = np.array([ORIGEN])
+baldosa = actor.cube(centers, colors=YELLOW, scales=baldosa_size)
+baldosa.SetPosition(0.5, 0.05, 0.5)
+
+# --- Crear la escena con los elementos
+scene = window.Scene()
+scene.add(ejes)
+scene.add(suelo)
+scene.add(baldosa)
+
+# --- Cambiar la posición de la cámara
+scene.GetActiveCamera().SetPosition(-50, 50, 80)
+
+# --- Cambiar el color del fondo
+scene.SetBackground(1, 1, 1)
+
+# ---- Crear la ventana
+window.show(scene, size=PANTALLA)
